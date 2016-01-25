47935d62d79a7a463eef04384188adf401ce12d2
DougFirErickson <adam.michael.erickson@gmail.com>
2016-01-24 23:48:33 -0800

    update

--- R/itcvarwin.R
+++ R/itcvarwin.R
@@ -1,4 +1,4 @@
-itc.varwin <- function(chm=NA, ht2rad=NA, geom='circle', res=1, num=TRUE, plots=FALSE, geoTIFF=FALSE) {
+itc.varwin <- function(chm=NA, ht2rad=NA, type='circle', res=1, num=TRUE, plots=FALSE, geoTIFF=FALSE) {
 
   require(rgdal)
   require(igraph)
@@ -11,13 +11,13 @@ itc.varwin <- function(chm=NA, ht2rad=NA, geom='circle', res=1, num=TRUE, plots=
     rgb(x[,1], x[,2], x[,3], maxColorValue=255)
   }
 
-  run.focal <- function(x, htz, rds, rad, type) {
-    htt <- htz[rds==rad | rds==rad-1]
-    x2  <- x > min(htt) & x < max(htt)
-    x3  <- x2 * x
+  run.focal <- function(chm=chm, hts=hts, rd2=rd2, rad, type=type) {
+    htt <- hts[rd2==rad | rd2==rad-1]
+    x2  <- chm > min(htt) & chm < max(htt)
+    x3  <- x2 * chm
     fun <- function(z, ...) ifelse(z[length(z)/2 + 0.5]==max(z), 1, NA)
     wts <- focalWeight(x=x3, d=rad, type=type)
-    itd <- focal(x=x, w=wts, fun=fun, na.rm=F, pad=rad, padValue=NA, NAonly=F)
+    itd <- focal(x=chm, w=wts, fun=fun, na.rm=F, pad=rad, padValue=NA, NAonly=F)
     return(itd)
   }
 
@@ -41,11 +41,11 @@ itc.varwin <- function(chm=NA, ht2rad=NA, geom='circle', res=1, num=TRUE, plots=
   message('Computing ', length(rd3), ' moving window(s)')
 
   if(length(rd3==1)) {
-    itc.out  <- run.focal(x=chm, htz=hts, rds=rd2, rad=rd3, type=geom)
+    itc.out  <- run.focal(chm=chm, hts=hts, rd2=rd2, rad=rd3, type=type)
   } else {
     itc.stk <- stack()
     for(i in 1:length(rd3)) {
-      itc.new  <- run.focal(x=chm, htz=hts, rds=rd2, rad=rd3[i], type=geom)
+      itc.new  <- run.focal(chm=chm, hts=hts, rd2=rd2, rad=rd3[i], type=type)
       itc.stk  <- stack(itc.stk, itc.new)
     }
     itc.out  <- stackApply(itc.stk, indices=c(1), fun=max, na.rm=T)
